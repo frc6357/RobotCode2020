82e15ed71a3f50fd4e33475ea1dcc18e22d027f2
Dave Wilson <dave@davewilsonphotography.com>
2020-01-07 17:59:23 -0600

    Add skeleton Ports.java

---
+++ blank_template/src/main/java/frc/robot/Ports.java
@@ -0,0 +1,13 @@
+// 
+// Definitions of robot hardware connections.
+//
+
+package frc.robot;
+
+public class Ports
+{
+    // ***************************
+    // I2C device addresses
+    // ***************************
+    public static final int i2cColorSensor   = 0x52;
+}
