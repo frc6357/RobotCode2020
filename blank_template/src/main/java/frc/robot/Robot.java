441409f6fe634941b94c24f9e594e1f342d05b2d
dpeeps <david.pieper123@gmail.com>
2020-01-09 16:46:14 -0600

    Files updated Files for the color sensor for continued testing have been updated.

--- blank_template/src/main/java/frc/robot/Robot.java
+++ blank_template/src/main/java/frc/robot/Robot.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.util.Color;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -30,8 +30,8 @@ public class Robot extends TimedRobot {
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
@@ -39,16 +39,16 @@ public class Robot extends TimedRobot {
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
-    
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
@@ -56,14 +56,15 @@ public class Robot extends TimedRobot {
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -78,13 +79,13 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    case kCustomAuto:
+      // Put custom auto code here
+      break;
+    case kDefaultAuto:
+    default:
+      // Put default auto code here
+      break;
     }
   }
 
@@ -99,10 +100,40 @@ public class Robot extends TimedRobot {
    * This function is called periodically during test mode.
    */
   @Override
-  public void testPeriodic() 
-  {
+  public void testPeriodic() {
     int valProx = m_colorSensor.getProximity();
     ColorSensorV3.RawColor valColor = m_colorSensor.getRawColor();
-    System.out.println("Proximity: " + valProx + " R:" + valColor.red + " G:" + valColor.green + " B:" + valColor.blue);
+    Color likelyColor = m_colorSensor.getColor();
+    int red = valColor.red;
+    int green = valColor.green;
+    int blue = valColor.blue;
+    // System.out.println("Proximity: " + valProx + " R:" + valColor.red + " G:" +
+    // valColor.green + " B:" + valColor.blue + " Likely color is:" +
+    // Integer.toHexString(likelyColor.hashCode()));
+    // System.out.printf("Proximity: %d R: %d G: %d B: %d Likely: %08x", valProx,
+    // valColor.red, valColor.green, valColor.blue, likelyColor.hashCode());
+
+    /*
+    This was our first algorithm and the problem was the green was overly higher than any other color we tested
+    Another problem problem we encountered was that any small variation got scaled into a much larger variation and any pure grey values would result in an error
+    int minimum = Math.min(Math.min(red, green), blue);
+    red -= minimum;
+    green -= minimum;
+    blue -= minimum;
+    int maximum = Math.max(Math.max(red, green), blue);
+    if(maximum)
+    {
+      red = red * 100 / maximum;
+      green = green * 100 / maximum;
+      blue = blue * 100 / maximum;
+    }
+    else
+    {
+      red = 0;
+      green = 0;
+      blue = 0;
+    }
+    System.out.println("R: " + red + " G: " + green + " B: " + blue + " Raw R: " + valColor.red + " G: " + valColor.green + " B: " + valColor.blue);
+    */
   }
 }
