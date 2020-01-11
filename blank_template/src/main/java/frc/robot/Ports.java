53d5f7ac4ebc726878d11b485b84407d0d14c55c
Dave Wilson <dave@davewilsonphotography.com>
2020-01-11 12:06:24 -0600

    Add basic control of the color wheel drive motor

--- blank_template/src/main/java/frc/robot/Ports.java
+++ blank_template/src/main/java/frc/robot/Ports.java
@@ -7,7 +7,19 @@ package frc.robot;
 public class Ports
 {
     // ***************************
-    // I2C device ports
+    // Drivetrain Connections
     // ***************************
     public static final int i2cColorSensor   = 0;
+
+    // ***************************
+    // Color Wheel Mechanism
+    // ***************************
+    public static final int motorColorWheel  = 21;  // CAN ID 21
+
+    // ***************************
+    // Driver Interface
+    // ***************************
+    public static final int OIDriverJoystick   = 0;
+
+    public static final int OIDriverLeftDrive  = 1;    // Left Joystick Y
 }
