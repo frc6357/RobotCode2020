b086c8fe96eb955cbe4a1f4b1cb2974d015f0970
David Pieper <david.pieper123@gmail.com>
2020-01-20 15:36:25 -0600

    Finished SK20Launcher SK20Launcher has been finished as well as add the Rev json file to be able to integrate their requirements easily into the launcher software.

--- src/main/java/frc/robot/TuningParams.java
+++ src/main/java/frc/robot/TuningParams.java
@@ -9,9 +9,18 @@ public final class TuningParams {
     public static final double DECEL_MAX_TOWARDS_FORWARD  = 0.01;
     public static final double DECEL_MAX_TOWARDS_BACKWARD = -0.01;
 
+
     public static final Color RGB_CYAN   = new Color(0.1799, 0.4451, 0.3799);
     public static final Color RGB_GREEN  = new Color(0.2199, 0.5200, 0.2300);
     public static final Color RGB_RED    = new Color(0.4199, 0.3899, 0.1699);
     public static final Color RGB_YELLOW = new Color(0.3100, 0.5300, 0.1499);
+
+    //////////////////////////////
+    // Launcher Params
+    //////////////////////////////
+    public static final int MAX_RPM                     = 5700;
+    public static final int P_VALUE                     = 1;
+    public static final int I_VALUE                     = 1;
+    public static final int D_VALUE                     = 1;
 
 }
--- src/main/java/frc/robot/subsystems/SK20Launcher.java
+++ src/main/java/frc/robot/subsystems/SK20Launcher.java
@@ -1,43 +1,98 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Ports;
 
 public class SK20Launcher extends SubsystemBase
 {
+    CANSparkMax launcherMotor = new CANSparkMax(Ports.ballLauncherMotor, MotorType.kBrushless);
+    CANPIDController PIDControl = launcherMotor.getPIDController();
+    CANEncoder launcherMotorEncoder = new CANEncoder(launcherMotor);
+
     public SK20Launcher()
     {
-
+        
     }
 
-    public void activateLauncher()
-    {
-
-    }
-
-    public void deactivateLauncher()
-    {
-
-    }
-
+    /**
+     * Since this motor doesn't run on a PID, and it just needs to run at a set speed there just needs to be a turn on and off method
+     */
     public void activateTransfer()
     {
 
     }
 
+    /**
+     * Since this motor doesn't run on a PID, and it just needs to run at a set speed there just needs to be a turn on and off method
+     */
     public void deactivateTransfer()
     {
 
     }
 
+    /**
+     * This returns the current encoder speed of the launcher
+     * @return The current encoder speed of the launcher
+     */
     public double getLauncherSpeed()
+    {
+        return 1.0;
+    }
+
+    /**
+     * This will return the encoder speed of the transfer motors
+     * @return The current speed of the transfer motors
+     */
+    public double getTransferSpeed()
     {
 
         return 1.0;
     }
 
-    public double getTransferSpeed()
+    /**
+     * Sets the PID values for the controller
+     * @param pVal The proportianal value
+     * @param iVal The integral value
+     * @param dVal The derivative value
+     */
+    public void setPIDValues(int pVal, int iVal, int dVal)
     {
+        PIDControl.setP(pVal);
+        PIDControl.setI(iVal);
+        PIDControl.setD(dVal);
+    }
 
-        return 1.0;
+    /**
+     * Sets the setpoint of the PID controller
+     * @param value The set double value
+     */
+    public void setSetpoint(double value)
+    {
+        PIDControl.setReference(value, ControlType.kVelocity);
+    }
+
+    /**
+     * It sets the hood according to whatever position it is passed
+     */
+    public void setHoodPosition(DoubleSolenoid.Value value)
+    {
+        
+    }
+
+    /**
+     * Checks to see what position the hood is in
+     * @return The value of the double solenoid
+     */
+    public DoubleSolenoid.Value getHoodPosition()
+    {
+        return Value.kForward;
     }
 }
