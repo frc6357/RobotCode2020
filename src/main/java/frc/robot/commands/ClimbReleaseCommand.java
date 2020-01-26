507b1b46eb29ac7a80ffabad1f6bcdf687589138
Shaun Kurian <shaunkurian345@gmail.com>
2020-01-25 18:18:23 -0600

    Updated Documentation and Formatting

--- src/main/java/frc/robot/commands/ClimbReleaseCommand.java
+++ src/main/java/frc/robot/commands/ClimbReleaseCommand.java
@@ -1,9 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.DriverStation;
@@ -13,53 +7,42 @@ import frc.robot.subsystems.SK20Climb;
 /**
  * An example command that uses an subsystem.
  */
-public class ClimbReleaseCommand extends CommandBase 
-{
-  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
-  private final SK20Climb m_subsystem;
-  private double time;
-  private String debugger;
+public class ClimbReleaseCommand extends CommandBase {
+    private final SK20Climb m_subsystem;
+    private double time;
+    private String debugger;
 
-  /**
-   * ClimbReleaseCommand tells that arm is released by pneumatic piston or not.
-   */
-  public ClimbReleaseCommand(SK20Climb subsystem) 
-  {
-    m_subsystem = subsystem;
-    time = DriverStation.getInstance().getMatchTime();
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
-  }
-
-  //TODO: don't alow outside of endgame
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() 
-  {
-    debugger = DriverStation.getInstance().getGameSpecificMessage();
-    
-    if(time <= 30 || debugger == "D")
-    {
-      m_subsystem.deployArm();
+    /**
+     * ClimbReleaseCommand tells that arm is released by pneumatic piston or not.
+     */
+    public ClimbReleaseCommand(SK20Climb subsystem) {
+        m_subsystem = subsystem;
+        time = DriverStation.getInstance().getMatchTime();
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
     }
-    else if(time > 30)
-    {
-      //For Testing Reasons
-      System.out.println("Time is not for climb");
-    }
-    else
-    {
-      //For Testing Reasons
-      System.out.println("Time is not known");
-    }
-    
 
-  }
+    // TODO: Don't allow outside of endgame
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        debugger = DriverStation.getInstance().getGameSpecificMessage();
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() 
-  {
-    return true;
-  }
+        if (time <= 30 || debugger == "D") {
+            m_subsystem.deployArm();
+        } else if (time > 30) {
+            // For Testing Reasons
+            System.out.println("Time is not for climb");
+        } else {
+            // For Testing Reasons
+            System.out.println("Time is not known");
+        }
+
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
 }
--- src/main/java/frc/robot/commands/WinchRobotCommand.java
+++ src/main/java/frc/robot/commands/WinchRobotCommand.java
@@ -1,74 +1,58 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SK20Climb;
+
 /**
  * An example command that uses an example subsystem.
  */
-public class WinchRobotCommand extends CommandBase 
-{
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final SK20Climb m_subsystem;
-  private boolean startWinch; //tells whether or not winch is turned on or not
-  private double time;
-  private String debugger;
+public class WinchRobotCommand extends CommandBase {
+    private final SK20Climb m_subsystem;
+    private boolean startWinch; // tells whether or not winch is turned on or not
+    private double time;
+    private String debugger;
 
-  /**
-   * WinchRobot command tells whether or not to winch or stop winching the robot.
-   * @param subsytem is the SK20Climb subsystem
-   * @param startMotor tells whether or not the winch motor should be on or off
-   */
-  public WinchRobotCommand(SK20Climb subsystem, Boolean startMotor) 
-  {
-    m_subsystem = subsystem;
-    startWinch = startMotor;
-    time = DriverStation.getInstance().getMatchTime();
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
-  }
+    /**
+     * WinchRobot command tells whether or not to winch or stop winching the robot.
+     * 
+     * @param subsytem   is the SK20Climb subsystem
+     * @param startMotor tells whether or not the winch motor should be on or off
+     */
+    public WinchRobotCommand(SK20Climb subsystem, Boolean startMotor) {
+        m_subsystem = subsystem;
+        startWinch = startMotor;
+        time = DriverStation.getInstance().getMatchTime();
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
+    }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() 
-  {
-    debugger = DriverStation.getInstance().getGameSpecificMessage();
-    
-    if(time <= 30 || debugger == "D")
-    {
-      //when true start winch
-      if(startWinch)
-      {
-        m_subsystem.startWinchRobot();
-      }
-      //when false stop winch
-      else 
-      {
-        m_subsystem.stopWinchRobot();
-      }
-    }
-    else if(time > 30)
-    {
-      //For Testing reasons
-      System.out.println("Time is not for climb");
-    }
-    else
-    {
-      //For Testing reasons
-      System.out.println("Time is not known");
-    }
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        debugger = DriverStation.getInstance().getGameSpecificMessage();
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() 
-  {
-    return true;
-  }
+        if (time <= 30 || debugger == "D") {
+            // when true start winch
+            if (startWinch) {
+                m_subsystem.startWinchRobot();
+            }
+            // when false stop winch
+            else {
+                m_subsystem.stopWinchRobot();
+            }
+        } else if (time > 30) {
+            // For Testing reasons
+            System.out.println("Time is not for climb");
+        } else {
+            // For Testing reasons
+            System.out.println("Time is not known");
+        }
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
 }
