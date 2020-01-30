9dac2fe890d43ca79ae24131a0d2933c872721db
David Pieper <david.pieper123@gmail.com>
2020-01-30 16:52:11 -0600

    Finished Launcher Class My sickness hiatus is finally over and I've finished the SK20Launcher class and is now pending a review to make sure that the code will actually work as intended.

--- src/main/java/frc/robot/subsystems/SK20Launcher.java
+++ src/main/java/frc/robot/subsystems/SK20Launcher.java
@@ -1,27 +1,38 @@
 package frc.robot.subsystems;
 
-// import com.revrobotics.CANEncoder;
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Ports;
 import frc.robot.TuningParams;
 import frc.robot.subsystems.base.BaseRoller;
 
+/**
+ * This is the launcher subsystem that controls everything that has to do with the launcher including the transfer from the handling system to the launcher
+ * as well the speed of the launcher and the position of the hood to change the set angle
+ */
 public class SK20Launcher extends SubsystemBase
 {
-    private CANSparkMax launcherMotor = new CANSparkMax(Ports.ballLauncherMotor, MotorType.kBrushless);
-    private CANPIDController PIDControl = launcherMotor.getPIDController();
+    private final CANSparkMax launcherMotor = new CANSparkMax(Ports.ballLauncherMotor, MotorType.kBrushless);
+    private final CANPIDController PIDControl = launcherMotor.getPIDController();
 
-    private CANSparkMax transferMotor = new CANSparkMax(Ports.ballLoaderMotor, MotorType.kBrushless);
-    private BaseRoller transferRoller = new BaseRoller(transferMotor, TuningParams.LOADER_MAX_SPEED);
-    // private CANEncoder launcherMotorEncoder = new CANEncoder(launcherMotor);
+    private final CANSparkMax transferMotor = new CANSparkMax(Ports.ballLoaderMotor, MotorType.kBrushless);   
+    private final BaseRoller transferRoller = new BaseRoller(transferMotor, TuningParams.LOADER_MAX_SPEED);
 
+    private final CANEncoder launcherMotorEncoder = new CANEncoder(launcherMotor);
+    private final CANEncoder transferMotorEncoder = new CANEncoder(transferMotor);
+
+    private final DoubleSolenoid hoodMover = new DoubleSolenoid(Ports.launcherHoodExtend, Ports.launcherHoodRetract);
+
+    /**
+     * This does nothing as everything is intialized inside of the class before the constructor is even called so that 
+     * we can guarantee 
+     */
     public SK20Launcher()
     {
         
@@ -40,16 +51,17 @@ public class SK20Launcher extends SubsystemBase
      */
     public void deactivateTransfer()
     {
-
+        transferRoller.setStop();
     }
 
     /**
      * This returns the current encoder speed of the launcher
+     * TODO: We need to test this method to see if it actually works the way that we think it does.
      * @return The current encoder speed of the launcher
      */
     public double getLauncherSpeed()
     {
-        return 1.0;
+        return launcherMotorEncoder.getVelocity();
     }
 
     /**
@@ -58,8 +70,7 @@ public class SK20Launcher extends SubsystemBase
      */
     public double getTransferSpeed()
     {
-
-        return 1.0;
+        return transferMotorEncoder.getVelocity();
     }
 
     /**
@@ -89,7 +100,7 @@ public class SK20Launcher extends SubsystemBase
      */
     public void setHoodPosition(DoubleSolenoid.Value value)
     {
-        
+        hoodMover.set(value);
     }
 
     /**
@@ -98,6 +109,16 @@ public class SK20Launcher extends SubsystemBase
      */
     public DoubleSolenoid.Value getHoodPosition()
     {
-        return Value.kForward;
+        return hoodMover.get();
+    }
+
+    /**
+     * This sets the speed of the motor using the PID control loop
+     * TODO: Check to see if this is actually setting the speed using the PID control loop rather than just setting the speed manually
+     * @param speed The speed that we want the motor to be runinng at
+     */
+    public void setLauncherSpeed(double speed)
+    {
+        launcherMotor.set(speed);
     }
 }
